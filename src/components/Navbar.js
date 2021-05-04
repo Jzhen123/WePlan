69a4078cdbd267c376ac8d3efc6888c3db496204
Jzhen123 <jiayuzheng01@gmail.com>
2021-05-04 17:06:55 -0400

    Changed firebase run so fix firebase deploy issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,29 +8,29 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
+                {/* <a className="navbar-brand" href="#">Navbar</a> */}
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            {/* <a className="nav-link" href="#">Features</a> */}
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            {/* <a className="nav-link" href="#">Pricing</a> */}
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <div className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown link
-                            </a>
+                            </div>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" onClick={logout}>Log Out</a></li>
+                                {/* <li><a className="dropdown-item" href="#">Action</a></li>
+                                <li><a className="dropdown-item" href="#">Another action</a></li> */}
+                                <li><button className="dropdown-item" onClick={logout}>Log Out</button></li>
                             </ul>
                         </li>
                     </ul>
