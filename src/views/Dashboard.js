a8a13416cb150a6d3ac9f9598b83422b229f5357
Jzhen123 <jiayuzheng01@gmail.com>
2021-05-07 11:27:27 -0400

    Added Fullcalendar componet to dashboard with a temporary Menu

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -17,14 +17,21 @@ function Dashboard() {
                 {
                     userData.name ?
                         <>
-                            <h1 className="fs-1">Welcome {userData.name}!</h1>
 
+                            <div className="row">
+                                <div className="col-2 mt-1">
+                                    <div className="card" style={{ height: '93vh' }}>
+                                        <h3>Welcome {userData.name}!</h3>
+                                    </div>
+                                </div>
+                                <div className="col-10">
 
-
-                            <FullCalendar
-                                plugins={[dayGridPlugin]}
-                                initialView="dayGridMonth"
-                            />
+                                    <FullCalendar
+                                        plugins={[dayGridPlugin]}
+                                        initialView="dayGridMonth"
+                                    />
+                                </div>
+                            </div>
 
 
 
