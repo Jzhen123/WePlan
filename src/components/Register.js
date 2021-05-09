257b93ac9ebd063653defb8ac4efdb1328fcd832
Jzhen123 <jiayuzheng01@gmail.com>
2021-05-08 22:48:20 -0400

    Set up other cases for formReducer

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,74 +1,3 @@
-// import React from 'react';
-// import validate from '../utilities/RegisterFormValidationRules';
-// import useForm from '../utilities/useForm';
-// import { useAuth } from '../utilities/AuthContext';
-// import { useHistory } from "react-router-dom";
-
-// const Register = () => {
-//   const {
-//     values,
-//     errors,
-//     handleChange,
-//     handleSubmit,
-//   } = useForm(submitForm, validate);
-//   const { register } = useAuth();
-//   const history = useHistory();
-
-//   function submitForm() {
-//     register(values);
-//     history.push("/");
-//   }
-
-//   return (
-//     <div className="section is-fullheight">
-//       <div className="container">
-//         <div className="column is-4 is-offset-4">
-//           <div className="box">
-//             <form onSubmit={handleSubmit} noValidate>
-
-//               <div className="field">
-//                 <label className="label">Name</label>
-//                 <div className="control">
-//                   <input autoComplete="off" className={`input ${errors.name && 'is-danger'}`} type="name" name="name" onChange={handleChange} value={values.name || ''} required />
-//                   {errors.name && (
-//                     <p className="help is-danger">{errors.name}</p>
-//                   )}
-//                 </div>
-//               </div>
-
-//               <div className="field">
-//                 <label className="label">Email Address</label>
-//                 <div className="control">
-//                   <input autoComplete="off" className={`input ${errors.email && 'is-danger'}`} type="email" name="email" onChange={handleChange} value={values.email || ''} required />
-//                   {errors.email && (
-//                     <p className="help is-danger">{errors.email}</p>
-//                   )}
-//                 </div>
-//               </div>
-
-//               <div className="field">
-//                 <label className="label">Password</label>
-//                 <div className="control">
-//                   <input className={`input ${errors.password && 'is-danger'}`} type="password" name="password" onChange={handleChange} value={values.password || ''} required />
-//                 </div>
-//                 {errors.password && (
-//                   <p className="help is-danger">{errors.password}</p>
-//                 )}
-//               </div>
-
-//               <button type="submit" className="button is-block is-info is-fullwidth">Sign Up</button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Register;
-
-// import { refineProps } from '@fullcalendar/common';
-import { hide } from '@popperjs/core';
 import React, { useEffect, useReducer } from 'react';
 import { useHistory } from 'react-router';
 import { useAuth } from '../utilities/AuthContext';
@@ -92,7 +21,6 @@ function Register() {
     const history = useHistory();
 
     useEffect(() => {
-        console.log(formState)
         if (formState.canSubmit === true) {
           register(formState.values)
           history.push('/')
--- src/utilities/reducers/formReducer.js
+++ src/utilities/reducers/formReducer.js
@@ -1,29 +1,15 @@
-import { useAuth } from "../AuthContext";
-
 export default function formReducer(state, action) {
     let tempState = { ...state }
-    // function init({initialFormType, }) {
-    //     return {
-    //         type: initialFormType,
-    //         values: {},
-    //         errors: {},
-    //         isSubmitting: false
-    //     }
-    // }
 
     switch (action.formType) {
         case 'REGISTER':
             switch (action.type) {
+                // OnChange for Register Form
                 case "onChange":
                     tempState.errors = {};
                     tempState.values[action.field] = action.payload
 
                     switch (action.field) {
-                        // case "email":
-                        //     if (!/\S+@\S+\.\S+/.test(action.payload)) {
-                        //         tempState.errors[action.field] = 'Email address format is invalid';
-                        //         break;
-                        //     }
                         case "password":
                             if (action.payload.length < 8) {
                                 tempState.errors[action.field] = 'Password must be 8 or more characters';
@@ -45,10 +31,6 @@ export default function formReducer(state, action) {
                         tempState.errors.email = "Email address is required";
                     } else if (!/\S+@\S+\.\S+/.test(tempState.values.email)) {
                         tempState.errors.email = "Email address format is invalid";
-                    } else {
-                        /* axiosHelper({}) Check if email is not already being used
-                                failureMethod: add some error
-                        */
                     }
                     if (!tempState.values.password) {
                         tempState.errors.password = "Password is required"
@@ -61,47 +43,11 @@ export default function formReducer(state, action) {
                     }
                     return tempState
 
+                case "LOGIN":
+                case "CREATE GROUP":
+
                 default:
                     return state;
             }
     }
-
-
-
-
-    // switch (action.function) {
-    //     case 'init': // Initializing Register specific object keys/values for formData 
-    //         return init(action.payload)
-
-    //     case 'onChange': // Storing current form values and then "frontend" error checking for simple rules/validation
-    //         if (!state.values.name) {
-    //             errors.name = 'Name is required';
-    //         }
-    //         if (!state.values.email) {
-    //             errors.email = "Email is required";
-    //         } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-    //             errors.email = "Email address format is invalid"
-    //         }
-    //         if (!state.values.password) {
-    //             errors.password = 'Password is required';
-    //         } else if (state.values.password < 8) {
-    //             errors.password = "Password must be 8 or more characters"
-    //         }
-
-    //         return { errors };
-    //     case 'onSubmit':
-    //         if (Object.keys(formData.errors).length === 0 && formData.isSubmitting) {
-    //             axiosHelper({
-    //                 // Check for unique emails
-    //                 data: formData.value.email,
-    //                 method: 'post',
-    //                 url: '///////////////', // Put URL in
-    //                 successMethod: {} // Return some success message
-    //             })
-    //         }
-
-    // }
-    // case 'LOGIN':
-    // case 'CREATEGROUP':
 }
-// }
