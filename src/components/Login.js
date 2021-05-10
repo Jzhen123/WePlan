0d33f197eef98fb6b94881f942fe2e6d9716ecc8
Jzhen123 <jiayuzheng01@gmail.com>
2021-05-10 11:35:19 -0400

    Fixed some warnings

--- src/components/Login.js
+++ src/components/Login.js
@@ -43,14 +43,14 @@ const Login = () => {
           <h2 className="mb-2">Start Planning!</h2>
           <form onSubmit={handleSubmit} >
 
-            <div class="form-floating mb-3 col-12">
-              <input type="text" class="form-control" name="username" onChange={(e) => handleChange(e)} id="usernameInput" placeholder="name@example.com" />
+            <div className="form-floating mb-3 col-12">
+              <input type="text" className="form-control" name="username" onChange={(e) => handleChange(e)} id="usernameInput" placeholder="name@example.com" />
               <label for="usernameInput">Email address</label>
               <div style={{ color: '#cc0000', height: '2vh', visibility: formState.errors.username ? 'visible' : 'hidden' }}>{formState.errors.username}</div>
             </div>
 
-            <div class="form-floating mb-3 col-12">
-              <input type="password" class="form-control" name="password" onChange={(e) => handleChange(e)} id="passwordInput" placeholder="Password" />
+            <div className="form-floating mb-3 col-12">
+              <input type="password" className="form-control" name="password" onChange={(e) => handleChange(e)} id="passwordInput" placeholder="Password" />
               <label for="passwordInput">Password</label>
               <div style={{ color: '#cc0000', height: '4vh', visibility: formState.errors.password ? 'visible' : 'hidden' }}>{formState.errors.password}</div>
             </div>
